4ee84e3e7acf0942ea3d446bdcd20be3315c3aa9
Mousa__Rm <mousasoccerlove@gmail.com>
2019-03-04 09:21:16 +0330

    دسترسی به اطلاعات داخل یو ار ال

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 // Do work here
 router.get('/', (req, res) => {
   // res.send('Hey! It works!');
-  const asd = { name: 'adlksa', age: 786, bool: true }
-  res.json(asd);
+  // const asd = { name: 'adlksa', age: 786, bool: true }
+  // res.json(asd);
   //میتونیم جی سون هم بفرستیم
   //ولی فقط میتونیم یک ریسپانس بفرستیم و گرنه ارور میده و میگه هدر ارسال شده
+
+  //حالا فرض کن میخوایم اطلاعات داخل یو ار ال رو بهش دسترسی پیدا کنیم جاش کجاست؟
+  req.query
 });
 
 module.exports = router;
