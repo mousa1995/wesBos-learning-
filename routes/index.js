9ec5483801cc5b7445459bb18759af4767c94de0
Mousa__Rm <mousasoccerlove@gmail.com>
2019-03-04 09:41:42 +0330

    نمونه کار با پارامس

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
+  res.send('Hey! It works!');
   // const asd = { name: 'adlksa', age: 786, bool: true }
   // res.json(asd);
   //میتونیم جی سون هم بفرستیم
   //ولی فقط میتونیم یک ریسپانس بفرستیم و گرنه ارور میده و میگه هدر ارسال شده
 
   //حالا فرض کن میخوایم اطلاعات داخل یو ار ال رو بهش دسترسی پیدا کنیم جاش کجاست؟
-  req.query
+  // req.query
+  //خب دلیل این که ما اصلا این کوری یا پارامس رو داریم چیه ؟ 
+  //دلیلش برمیگرده به بادیپارسر که میدلوری هست قبل از این که حتی به راه ها برسیم
+  //میاد کل بادی ریکوست رو تبدیل به جیسون میکنه
 });
+    router.get('/reverse/:name', (req, res) => {
+      const reverse = [...req.params.name];
+      res.json(req.params);
+    });
 
 module.exports = router;
